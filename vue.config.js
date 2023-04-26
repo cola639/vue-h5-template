264248f38651f5efb0d047f7c21f5a5288b3d935
cola639 <337507950@qq.com>
2023-04-27 03:18:38 +0800

    add

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
-const isProd = process.env.NODE_ENV === 'production'
 const path = require('path')
-const publicPath = '/web/'
-const themePath = path.join(__dirname, './src/styles/theme/overwrite.less') // vant theme path
 const title = 'Title' // page title
+const themePath = path.join(__dirname, './src/styles/theme/overwrite.less') // vant theme path
+const isProd = process.env.NODE_ENV === 'production'
+const publicPath = '/web/'
 
 function resolve(dir) {
   return path.join(__dirname, dir)
