2ac7082f5ebb81b341c307a32ad8d966b4ac8e8f
cola639 <337507950@qq.com>
2023-05-05 11:05:55 +0800

    add: 双指放大

--- public/index.js
+++ public/index.js
@@ -1,27 +1,58 @@
 // limit double click
-document.documentElement.addEventListener(
+// document.documentElement.addEventListener(
+//   'touchstart',
+//   function (event) {
+//     if (event.touches.length > 1) {
+//       event.preventDefault()
+//     }
+//   },
+//   false
+// )
+
+// let lastTouchEnd = 0
+// document.documentElement.addEventListener(
+//   'touchend',
+//   function (event) {
+//     const now = Date.now()
+//     if (now - lastTouchEnd <= 300) {
+//       event.preventDefault()
+//     }
+//     lastTouchEnd = now
+//   },
+//   false
+// )
+
+// document.addEventListener('gesturestart', function (event) {
+//   event.preventDefault()
+// })
+
+let lastTouchEnd = 0
+document.addEventListener(
   'touchstart',
-  function (event) {
+  event => {
     if (event.touches.length > 1) {
       event.preventDefault()
     }
   },
-  false
+  { passive: false }
 )
 
-let lastTouchEnd = 0
-document.documentElement.addEventListener(
+document.addEventListener(
   'touchend',
-  function (event) {
+  event => {
     const now = Date.now()
     if (now - lastTouchEnd <= 300) {
       event.preventDefault()
     }
     lastTouchEnd = now
   },
-  false
+  { passive: false }
 )
 
-document.addEventListener('gesturestart', function (event) {
-  event.preventDefault()
-})
+document.addEventListener(
+  'gesturestart',
+  event => {
+    event.preventDefault()
+  },
+  { passive: false }
+)
