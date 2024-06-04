ca5d0cabddb602c865df88451e511171308d7a12
cola639 <337507950@qq.com>
2024-06-04 14:07:13 +0800

    update: 超长字符省略组件

---
+++ src/views/Demo/text/text_v1.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- 当一个div使用 -->
+  <el-tooltip placement="top-start" effect="light">
+    <div slot="content" :style="{ fontSize: '25px' }">
+      {{ item.address }}
+    </div>
+    <div class="address">{{ item.address }}</div>
+  </el-tooltip>
+</template>
+
+<script>
+export default {
+  name: 'text_v1',
+  components: {},
+  data() {
+    return {}
+  },
+  props: {
+    width: { type: Number, required: true }
+  },
+  created() {},
+  mounted() {},
+  beforeDestroy() {},
+  computed: {},
+  watch: {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.address {
+  width: 30%;
+  height: 100%;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 1;
+  overflow: hidden;
+}
+</style>
